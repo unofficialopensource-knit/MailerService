06e61611aafd1e373dfb0f48562901186dc4bd51
Mayank Pathak <onlinejudge95@gmail.com>
2024-03-23 05:29:40 +0530

    Updates Schema to include entire config schema for mail module

--- pkg/server/schema.go
+++ pkg/server/schema.go
@@ -1,6 +1,25 @@
 package server
 
 type MailRequestSchema struct {
+	UseServerMail    bool                    `json:"use_server_mail" binding:"required"`
+	CustomMailConfig *CustomMailConfigSchema `json:"custom_mail_config"`
+	TemplateSchema   *TemplateSchema         `json:"template_schema" binding:"required"`
+}
+
+type CustomMailConfigSchema struct {
+	Host         string `json:"host,omitempty"`
+	Port         string `json:"port,omitempty"`
+	Username     string `json:"username,omitempty"`
+	Password     string `json:"password,omitempty"`
+	SMTPIdentity string `json:"smtp_identity,omitempty"`
+}
+
+type TemplateSchema struct {
+	Type    string                 `json:"type" binding:"required"`
+	Context *TemplateContextSchema `json:"context" binding:"required"`
+}
+
+type TemplateContextSchema struct {
 	Name          string `json:"name" binding:"required"`
 	Email         string `json:"email" binding:"required"`
 	ContactNumber string `json:"contact_number" binding:"required"`
