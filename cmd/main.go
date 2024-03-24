501f0fa37b90b144226727f67509e5e46370cdd4
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2024-03-24 14:18:18 +0530

    Add support for serverless framework (#12)
    * Updates action version and remove commented code
    
    * Adds changes for running gin server on lambda
    
    * Renames ping to health
    
    * Fixes failing build and moves all code into singel module
    
    * Fixes linting failure
    
    * Fixes test stage
    
    * Fixes failing test

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,61 @@
 package main
 
 import (
-	"github.com/unofficialopensource-knit/MailerService/pkg/server"
+	"context"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
+	"github.com/kelseyhightower/envconfig"
 )
 
+var routerLambda *ginadapter.GinLambda
+
+type Config struct {
+	Environment string `encvonfig:"environment"`
+	BindAddress string `envconfig:"bind_addr"`
+}
+
+func AppFactory(mode string) *gin.Engine {
+	switch strings.ToUpper(strings.TrimSpace(mode)) {
+	case "DEBUG":
+		gin.SetMode(gin.DebugMode)
+	case "TEST":
+		gin.SetMode(gin.TestMode)
+	case "RELEASE":
+		gin.SetMode(gin.ReleaseMode)
+	default:
+		panic("Mode not supported for running the server")
+	}
+
+	gin.DisableConsoleColor()
+
+	router := gin.Default()
+	router.GET("/health", HealthHandler)
+	return router
+}
+
+func HealthHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"message": "healthy"})
+}
+
 func main() {
-	conf := server.LoadConfig()
+	var config Config
+	err := envconfig.Process("mailer", &config)
+	if err != nil {
+		log.Panicf("Got error while loading config %v", err.Error())
+	}
 
-	router := server.AppFactory(conf.Environment)
+	router := AppFactory(config.Environment)
+	routerLambda = ginadapter.New(router)
 
-	router.Run(conf.BindAddress)
+	lambda.Start(LambdaHandler)
+}
+
+func LambdaHandler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return routerLambda.ProxyWithContext(ctx, req)
 }
