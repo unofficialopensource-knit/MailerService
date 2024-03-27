d906721476f138f92787bf8db3950348d48ec94f
Mayank Pathak <onlinejudge95@gmail.com>
2024-03-27 16:04:43 +0530

    Adds Mailservice

---
+++ pkg/service/mail.go
@@ -0,0 +1,62 @@
+package service
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/smtp"
+	"text/template"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/unofficialopensource-knit/MailerService/pkg/schema"
+)
+
+func HandleContactUs(config schema.Config, body schema.MailSchema) []string {
+	recipients := []string{config.ContactUsDefaultRecipient}
+	return recipients
+}
+
+func SendMail(payload schema.MailRequestSchema) {
+	var conf schema.Config
+	err := envconfig.Process("mailer", &conf)
+
+	if err != nil {
+		log.Panicf("Got error while loading config %v", err.Error())
+	}
+
+	var recipients []string
+	var templateContext map[string]string
+	switch payload.Schema.TemplateType {
+	case "FORGOT_PASSWORD":
+		panic("Service not yet implemented")
+	case "CONTACT_US":
+		recipients = HandleContactUs(conf, payload.Schema)
+		templateContext = map[string]string{
+			"Name":          payload.Schema.TemplateContext.Name,
+			"Email":         payload.Schema.TemplateContext.Email,
+			"ContactNumber": payload.Schema.TemplateContext.ContactNumber,
+			"UserType":      payload.Schema.TemplateContext.UserType,
+			"Message":       payload.Schema.TemplateContext.Message,
+		}
+	case "WELCOME_MAIL":
+		panic("Service not yet implemented")
+	default:
+		panic("Service not yet implemented")
+	}
+
+	mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	var body bytes.Buffer
+	body.Write([]byte(fmt.Sprintf("Subject: New Lead  \n%s\n\n", mimeHeaders)))
+
+	tpl, _ := template.ParseFiles("templates/contact_us.html")
+	tpl.Execute(&body, templateContext)
+
+	if payload.UseServerDefaultConfig {
+		auth := smtp.PlainAuth(conf.SMTPIdentity, conf.SMTPUsername, conf.SMTPPassword, conf.SMTPHost)
+		err = smtp.SendMail(conf.SMTPHost+":"+conf.SMTPPort, auth, conf.SMTPUsername, recipients, body.Bytes())
+		if err != nil {
+			return
+		}
+	}
+	panic("Not supported custom SMTP Creds")
+}
