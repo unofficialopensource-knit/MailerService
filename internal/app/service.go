9070acb6c08bce7c86fdd908a9a8cb4f51192386
Mayank Pathak <onlinejudge95@gmail.com>
2024-05-02 19:15:45 +0530

    Adds template for welcome email

--- internal/app/service.go
+++ internal/app/service.go
@@ -22,8 +22,7 @@ func NewService(conf HTTPConfig) *Service {
 	}
 }
 
-func (s *Service) SendContactUsMail(payload ContactUsInputSchema) error {
-	subject := "New Lead"
+func (s *Service) SendContactUsMail(payload ContactUsInput) error {
 	mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 
 	h := hermes.Hermes{
@@ -56,6 +55,62 @@ Has reached out with the following query
 
 {{ .Message }}
 				`,
+		},
+	}
+
+	s.Body.Write([]byte(fmt.Sprintf("Subject: %s  \n%s\n\n", "New Lead", mimeHeaders)))
+
+	emailBody, err := h.GenerateHTML(email)
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+
+	err = os.WriteFile(templatePath, []byte(emailBody), 0666)
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+
+	tpl, _ := template.ParseFiles(templatePath)
+	err = tpl.Execute(&s.Body, templateContext)
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+	serverAuth := smtp.PlainAuth(s.Config.SMTPIdentity, s.Config.SMTPUsername, s.Config.SMTPPassword, s.Config.SMTPHost)
+
+	err = smtp.SendMail(s.Config.SMTPHost+":"+s.Config.SMTPPort, serverAuth, s.Config.SMTPUsername, []string{s.Config.ContactUsDefaultRecipient}, s.Body.Bytes())
+	if err != nil {
+		slog.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (s *Service) SendWelcomeMail(payload WelcomeInput) error {
+	subject := "Welcome to WeCoach.AI -Train Smarter not Harder"
+	mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+
+	h := hermes.Hermes{
+		Product: hermes.Product{
+			Name: "Hermes",
+			Link: "http://wecoach.ai",
+			Logo: "http://wecoach.ai/static/images/logo.png",
+		},
+	}
+	templatePath := "/tmp/welcome.html"
+	templateContext := map[string]string{
+		"Name": payload.Name,
+	}
+	email := hermes.Email{
+		Body: hermes.Body{
+			Name:   "{{ .Name }}",
+			Intros: []string{},
+			Outros: []string{
+				"We're committed to helping you unlock your full potential and achieve your fitness goals. If you have any questions or need assistance, don't hesitate to reach out to our support team at wecoach.ai@gmail.com or contact +91-9953836512",
+				"Once again, welcome to WeCoach.AI! Get ready to transform your fitness and unleash your best self.",
+			},
 		},
 	}
 
