35b9c4ce91b6a65c2a7d832df8c57ec2ee84b023
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2024-05-03 17:15:00 +0530

    Updates email for coach and student (#56)

--- internal/app/service.go
+++ internal/app/service.go
@@ -96,7 +96,9 @@ Has reached out with the following query
 }
 
 func (s *Service) SendWelcomeMail(payload WelcomeInput) error {
-	var intro []string
+	var intro string
+	var content string
+	var outro string
 	signature := `
 	Best regards,
 
@@ -107,21 +109,50 @@ func (s *Service) SendWelcomeMail(payload WelcomeInput) error {
 	WeCoach.AI
 	`
 	if payload.UserType == "student" {
-		intro = []string{
-			"Welcome to WeCoach.AI. We're thrilled to have you on board and excited to embark on this journey toward achieving your fitness goals together. Here's a brief overview to help you navigate through your dashboard.",
-			"1. **Dashboard Navigation**: Your dashboard is your central hub for accessing all our services and tracking your progress. You'll find tabs for your Profile Information, Update Profile and AI tracker making it easy to stay organized and focused on your goals.",
-			"2. **AI-Powered Fitness Test**: One of the unique features of our platform is our AI-powered fitness test FlexScore-AI. Our AI Fitness Test analyses various aspects of fitness, including strength, endurance and stamina by using body weight exercises as a metrics.",
-			"3. **Customized Workouts and Nutrition Plans**: Say goodbye to generic fitness plans! Our platform offers personalized workout routines and nutrition plans tailored to your specific needs and goals based on your fitness test result. Whether you're looking to build strength, improve endurance, or lose weight, we've got you covered.",
-			"4. We Wellness : Counseling Services for Holistic Well-being. At WeCoach.AI, we understand that success goes beyond physical performance. We are thrilled to introduce 'We Wellness', our counseling service designed to support the mental health and overall well-being of students and their parents. Our expert team of counsellors will help you stay motivated and tackle your day to day problems seamlessly.",
-			"5. FAI Score: The FAI Score is an indication of your overall fitness level and is bound to improve once you regularly follow the workout regime and nutrition plans with WeCoach.AI",
-			"To access your current fitness regime download the AI-powered report card after your fitness test on a monthly basis.",
-		}
+		intro = "Welcome to WeCoach.AI. We're thrilled to have you on board and excited to embark on this journey toward achieving your fitness goals together. Here's a brief overview to help you navigate through your dashboard."
+		content = `
+		1. **Dashboard Navigation** - Your dashboard is your central hub for accessing all our services and tracking your progress. You'll find tabs for your Profile Information, Update Profile and AI tracker making it easy to stay organized and focused on your goals.
+		2. **AI-Powered Fitness Test** - One of the unique features of our platform is our AI-powered fitness test FlexScore-AI. Our AI Fitness Test analyses various aspects of fitness, including strength, endurance and stamina by using body weight exercises as a metrics.
+		3. **Customized Workouts and Nutrition Plans** - Say goodbye to generic fitness plans! Our platform offers personalized workout routines and nutrition plans tailored to your specific needs and goals based on your fitness test result. Whether you're looking to build strength, improve endurance, or lose weight, we've got you covered.
+		4. We Wellness - Counseling Services for Holistic Well-being. At WeCoach.AI, we understand that success goes beyond physical performance. We are thrilled to introduce 'We Wellness', our counseling service designed to support the mental health and overall well-being of students and their parents. Our expert team of counsellors will help you stay motivated and tackle your day to day problems seamlessly.
+		5. FAI Score - The FAI Score is an indication of your overall fitness level and is bound to improve once you regularly follow the workout regime and nutrition plans with WeCoach.AI
+		`
+		outro = `
+		To access your current fitness regime download the AI-powered report card after your fitness test on a monthly basis.
+
+		We're committed to helping you unlock your full potential and achieve your fitness goals. If you have any questions or need assistance, don't hesitate to reach out to our support team at wecoach.ai@gmail.com or contact +91-9953836512. 
+
+Once again, welcome to WeCoach.AI! Get ready to transform your fitness and unleash your best self.
+		`
 	} else {
-		intro = []string{
-			"Welcome to WeCoach.AI - the ultimate platform for coaches dedicated to empowering athletes and driving performance excellence using artificial intelligence. We're thrilled to have you join our community of passionate coaches, and we're excited to support you in your mission to help athletes reach their full potential.",
-			"As you begin your journey with us, we want to ensure that you have all the tools and resources you need to succeed.",
-			"Here's a brief overview to help you navigate through your dashboard and make the most of your experience with WeCoach.AI",
-		}
+		intro = `
+		Welcome to WeCoach.AI - the ultimate platform for coaches dedicated to empowering athletes and driving performance excellence using artificial intelligence. We're thrilled to have you join our community of passionate coaches, and we're excited to support you in your mission to help athletes reach their full potential.
+
+		Here's a brief overview to help you navigate through your dashboard and make the most of your experience with WeCoach.AI
+		`
+		content = `
+		1. **Dashboard Overview** - Your dashboard is your command center for managing your coaching activities, and engaging with your athletes. Navigate seamlessly through tabs for athlete profiles, training programs, progress tracking, and more, all in one centralized location.
+		2. **AI-Powered Performance Analysis** - Gain a competitive edge with our AI-powered Fitness test FlexScore AI. Our advanced algorithms analyze athlete data, providing actionable insights into areas of strength, improvement opportunities, and personalized training recommendations.
+		3. **Customized Training Programs** - Design tailored training programs to meet the unique needs and goals of each athlete. Our platform allows you to create personalized workout routines, set goals and track progress.
+		4. **Comprehensive Athlete Profiles** - Access detailed athlete profiles that provide valuable insights into performance metrics, training history, injury status, and more.
+		5. **Community Engagement and Networking** - Join our vibrant community of coaches and sports professionals to share best practices, exchange ideas, and network with like-minded individuals.
+
+		### BENEFITS OF USING [WECOACH.AI](http://wecoach.ai)
+
+		|Aspect|Traditional Coaching|WeCoach.AI Coaching|
+		|------|--------------------|-------------------|
+		|Data Collection|Manual recording of data|Automated Video Recording|
+		|Personalization|Limited customization|Highly personalized plans|
+		|Efficiency|Prone to human error|Accurate and efficient|
+		|Feedback|Delayed and subjective|Instant and objective|
+		|Injury Prevention|Reacts after injury occurs|Identifies risks beforehand|
+		|Continuous Improvement|Limited adaptation|Constantly learns and evolves|
+		|Remote Coaching|Requires in-person presence|Remote monitoring possible|
+
+		`
+		outro = `
+		If you have any questions or need assistance, please don't hesitate to reach out to our support team at wecoach.ai@gmail.com or 9953836512.
+		`
 	}
 
 	mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
@@ -137,16 +168,24 @@ func (s *Service) SendWelcomeMail(payload WelcomeInput) error {
 	templatePath := "/tmp/welcome.html"
 	templateContext := map[string]string{
 		"Name": payload.Name,
+		"Intro": intro,
+		"Content": content,
+		"Outro": outro,
+		"Signature": signature,
 	}
 	email := hermes.Email{
 		Body: hermes.Body{
-			Name:   "{{ .Name }}",
-			Intros: intro,
-			Outros: []string{
-				"We're committed to helping you unlock your full potential and achieve your fitness goals. If you have any questions or need assistance, don't hesitate to reach out to our support team at wecoach.ai@gmail.com or contact +91-9953836512",
-				"Once again, welcome to WeCoach.AI! Get ready to transform your fitness and unleash your best self.",
-			},
-			Signature: signature,
+			FreeMarkdown: `
+			{{ .Name }}
+
+			{{ .Intro }}
+
+			{{ .Content }}
+
+			{{ .Outro }}
+
+			{{ .Signature }}
+			`,
 		},
 	}
 
